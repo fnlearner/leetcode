60402f9d6c24dd42b28fb6706aa5f63d96428d63
JS <38586156+fnlearner@users.noreply.github.com>
2019-12-27 11:00:35 +0800

    add

---
+++ algorithm/containsNearbyAlmostDuplicate.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @param {number} t
+ * @return {boolean}
+ */
+var containsNearbyAlmostDuplicate = function(nums, k, t) {
+
+
+};
+
+containsNearbyAlmostDuplicate( [1,2,3,1],3,0)
