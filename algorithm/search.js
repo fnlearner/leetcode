30f913945d58ae222a7230a9fe4bd17ed956f17a
zjs <17350006490@163.com>
2020-04-27 15:22:55 +0800

    feat:增加一个二分查找搜索旋转数组

---
+++ algorithm/search.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    const len = nums.length;
+    if(!len) return -1
+    if(len === 1) return nums[0] === target ?0 :-1;
+    let left =0,right = len-1
+    while(left <= right){
+        let mid = Math.floor((left+right)/2)
+        console.log(mid)
+        if(nums[mid] === target) return mid;
+        //因为数组是有序的，所以元素进行旋转数组也是有序的，判断头节点是否小于尾节点就可以得出是否是有序
+        if(nums[0] <= nums[mid]){
+            //如果targe在 0到mid之间,缩小搜索范围
+            if(target >= nums[0] && target <= nums[mid]){
+                right = mid-1
+            }else{
+                left = mid+1
+            }
+        }else{
+            if(target > nums[mid] && target <= nums[len-1]){
+                left = mid+1;
+            }else{
+                right =mid-1
+            }
+        }
+    }
+    return -1
+};
