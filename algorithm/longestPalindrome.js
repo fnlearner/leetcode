eeda3398c71d4e45fc93696f1ecba7406220f0b6
JS <38586156+fnlearner@users.noreply.github.com>
2020-05-07 18:17:00 +0800

    区分寻找最长回文子串与寻找最长回文串算法

--- algorithm/longestPalindrome.js
+++ algorithm/longestPalindrome.js
@@ -1,38 +1,57 @@
 /**
  * @param {string} s
  * @return {number}
- * 最长回文串
+ * 最长回文子串
  */
 var longestPalindrome = function(s) {
-    // let str = '*'
-    // let ans =1;
-    // for(let item of s){
-    //     str+=`${item}*`
-    // }
-    // let index =0
-    // while(str[index]){
+    let str = '*'
+    let ans =1;
+    for(let item of s){
+        str+=`${item}*`
+    }
+    let index =0
+    while(str[index]){
     
-    //     let left = index-1
-    //     let right = index+1
-    //     let cur = str[index] === '*'?0:1;
-    //     while(str[left]&&str[right]){
-    //         if(str[left] === '*')--left
-    //         if(str[right] === '*')++right
-    //         if(str[left] === str[right]){
-    //             left--;
-    //             right++;
-    //             cur+=2
-    //         }else{
-    //             break
-    //         }
-    //     }
-    //     ans = Math.max(ans,cur)
-    //     index++
-    // }
+        let left = index-1
+        let right = index+1
+        let cur = str[index] === '*'?0:1;
+        while(str[left]&&str[right]){
+            if(str[left] === '*')--left
+            if(str[right] === '*')++right
+            if(str[left] === str[right]){
+                left--;
+                right++;
+                cur+=2
+            }else{
+                break
+            }
+        }
+        ans = Math.max(ans,cur)
+        index++
+    }
     // console.log(ans)
-    // return ans
+    return ans
      // const set = new Set()
-     const map = new Map()
+    //  const map = new Map()
+    //  for(let item of s){
+    //      map[item] = ++map[item]||1
+    //  }
+    //  console.log(Object.values(map))
+    //  const len = s.length
+    //  const res = Object.values(map).reduce((sum,cur)=>{
+    //      const add = cur&1 ?cur-1:cur
+    //      return sum += add
+    //  },0) ;
+
+    //  return res<len?res+1:res
+};
+/**
+ * 
+ * @param {string} s
+ * 最长回文串 
+ */
+const _longestPalindrome = s =>{
+         const map = new Map()
      for(let item of s){
          map[item] = ++map[item]||1
      }
@@ -42,7 +61,7 @@ var longestPalindrome = function(s) {
          const add = cur&1 ?cur-1:cur
          return sum += add
      },0) ;
-     return res<len?res+1:res
-};
 
+     return res<len?res+1:res
+}
 longestPalindrome('abccccdd')
